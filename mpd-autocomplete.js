6440fb739a30b9c3ded2a67ca3c02e9769302f66
Mike Duffey <mpduffey@gmail.com>
2015-12-12 17:28:29 +0000

    Sat Dec 12 17:28:29 UTC 2015

--- mpd-autocomplete.js
+++ mpd-autocomplete.js
@@ -1,23 +1,35 @@
 angular.module('mpd.autocomplete', [])
 .directive('mpdAutocomplete', function(){
 	return {
-		require:	'ngModel',
-		link:		function($scope, $element, $attrs, ngModel) {
-						ngModel.$render = function() {
-							$element.val(ngModel.$viewValue || '');
-						};
-						$element.autocomplete({
-							autoFocus:		$attrs.autoFocus || true,
-							delay:			$attrs.delay || false,
-							disabled:		$attrs.autocompleteDisabled || false,
-							minLength:		$attrs.minLength || 3,
-							source:			$scope[$attrs.source] || $scope.attrs,
-							select:			$attrs.select || function(ev, ui) {
-																$scope.$apply(function() {
-																	ngModel.$setViewValue(ui.item.value);
-																});
-															}
+		require: 'ngModel',
+			link: function($scope, $element, $attrs, ngModel) {
+				ngModel.$render = function() {
+					$element.val(ngModel.$viewValue || '');
+				};
+				$element.autocomplete({
+					autoFocus: $attrs.autoFocus || true,
+					delay: $attrs.delay || false,
+					disabled: $attrs.autocompleteDisabled || false,
+					minLength: $attrs.minLength || 3,
+					source: function(req, res) {
+						res($.map($scope[$attrs.source].data, function(item) {
+							if (item.ObjectName.toLowerCase().indexOf(req.term.toLowerCase()) > -1) {
+								return {
+									label: item.ObjectName,
+									value: item.ObjectID
+								};
+							}
+						}))
+					},
+					select: $attrs.select || function(ev, ui) {
+							ngModel.$setViewValue(ui.item.label);
+					},
+					focus: $attrs.focus || function(ev, ui) {
+						$scope.$apply(function() {
+							ngModel.$setViewValue(ui.item.label);
 						});
-		}
+					}
+				});
+			}
 	};
 });
